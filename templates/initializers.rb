bfaf273286bd2f7561e3a95a92800b2823b608fa
Collin Schaafsma <collin@quickleft.com>
2010-10-24 23:15:26 -0600

    adding haml default format to html5

--- templates/initializers.rb
+++ templates/initializers.rb
@@ -1,6 +1,12 @@
-create_file 'config/initializers/active_record.rb' do
+initializer('active_record.rb') do
 <<-'FILE'
 # This forces you to set attr_accessible in all your models
 ActiveRecord::Base.send(:attr_accessible, nil)
+FILE
+end
+
+initializer('haml.rb') do
+<<-'FILE'
+Haml::Template.options[:format] = :html5
 FILE
 end
