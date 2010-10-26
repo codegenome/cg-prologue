2d2d34df38ea2b3005ecf2663abb8c384e45faac
Collin Schaafsma <collin@quickleft.com>
2010-10-25 22:00:26 -0600

    fix typo in deleted_at scope

--- templates/devise.rb
+++ templates/devise.rb
@@ -44,7 +44,7 @@ create_file 'app/models/user.rb' do
 class User < ActiveRecord::Base
   devise :database_authenticatable, :token_authenticatable, :recoverable, :rememberable, :trackable, :validatable, :confirmable
   validates_presence_of :name
-  validates_uniqueness_of :name, :email, :case_sensitive => false, :scope => deleted_at
+  validates_uniqueness_of :name, :email, :case_sensitive => false, :scope => :deleted_at
   attr_accessible :name, :email, :password, :password_confirmation, :remember_me
   has_friendly_id :name, :use_slug => true, :strip_non_ascii => true
 
