483605e702a995cf3b3d65b0dc03e27cf3db7f7e
Collin Schaafsma <collin@quickleft.com>
2010-10-26 02:31:12 -0600

    update gemfile and prologue gemspec file

--- prologue.gemspec
+++ prologue.gemspec
@@ -19,7 +19,7 @@ Gem::Specification.new do |s|
   s.add_development_dependency "bundler", ">= 1.0.0"
   s.add_development_dependency "cucumber"
   s.add_development_dependency "aruba" 
-  s.add_development_dependency "rspec", "~> 2.0.0.beta.22"
+  s.add_development_dependency "rspec", "~> 2.0.0"
 
   s.files        = `git ls-files`.split("\n")
   s.executables  = `git ls-files`.split("\n").map{|f| f =~ /^bin\/(.*)/ ? $1 : nil}.compact
--- templates/gemfile.rb
+++ templates/gemfile.rb
@@ -1,9 +1,9 @@
 run 'rm Gemfile'
 create_file 'Gemfile', "source 'http://rubygems.org'\n"
-gem "rails", "3.0.0"
+gem "rails", "~> 3.0.0"
 gem "sqlite3-ruby", :require => "sqlite3"
 if ENV['PROLOGUE_AUTH']
-  gem 'devise', ">= 1.1.2"
+  gem 'devise', ">= 1.1.3"
 end
 if ENV['PROLOGUE_ROLES']
   gem 'cancan'
@@ -16,17 +16,17 @@ gem "haml", ">= 3.0.21"
 gem "rails3-generators", :group => :development
 gem "hpricot", :group => :development
 gem "ruby_parser", :group => :development
-gem "rspec-rails", "2.0.0.beta.22", :group => [:test, :development]
+gem "rspec-rails", "~> 2.0.0", :group => [:test, :development]
 gem "mocha", :group => [:test]
 gem "factory_girl_rails", :group => [:test, :cucumber]
 gem "faker", :group => [:test]
 gem "autotest", :group => [:test]
 gem "autotest-rails", :group => [:test]
 gem "thin", :group => [:test, :cucumber, :development]
-gem "cucumber", :git => "git://github.com/aslakhellesoy/cucumber.git", :group => [:cucumber]
-gem "database_cleaner", :git => "git://github.com/bmabey/database_cleaner.git", :group => [:test, :cucumber]
-gem "cucumber-rails", :git => "git://github.com/aslakhellesoy/cucumber-rails.git", :group => [:cucumber]
-gem "capybara", "0.3.9", :group => [:cucumber]
+gem "cucumber", :group => [:cucumber]
+gem "database_cleaner", :group => [:test, :cucumber]
+gem "cucumber-rails", :group => [:cucumber]
+gem "capybara", "0.4.0", :group => [:cucumber]
 gem "launchy", :group => [:cucumber]
 gem "timecop", :group => [:test, :cucumber]
 gem "pickle", :group => [:test, :cucumber]
