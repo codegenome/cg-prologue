97e78cdc36938a1381516ab34b456819155a49d9
Collin Schaafsma <collin@quickleft.com>
2010-10-25 22:51:34 -0600

    adding cuke to test for devise default password reset

--- templates/devise/cucumber.rb
+++ templates/devise/cucumber.rb
@@ -73,3 +73,28 @@ inject_into_file 'features/support/paths.rb', :after => "case page_name\n" do
         '/users/sign_in'
 FILE
 end
+
+create_file 'features/forgot_password.feature' do
+<<-'FILE'
+Feature: Forgot password
+  As an user
+  I want to secure way to reset my password
+  So that I can still login if I forget it
+
+  Scenario: Follow forget your password link
+    Given the following user records
+    | email                |
+    | forgot@quickleft.com |
+    When I am on the login page
+    And I follow "Forgot your password?"
+    And I fill in "Email" with "forgot@quickleft.com"
+    And I press "Send me reset password instructions"
+    Then I should see "You will receive an email with instructions about how to reset your password in a few minutes."
+    And 1 emails should be delivered to forgot@quickleft.com
+    When I click the first link in the email
+    And I fill in "Password" with "myNewP@ssword"
+    And I fill in "Password confirmation" with "myNewP@ssword"
+    And I press "Change my password"
+    Then I should see "Your password was changed successfully. You are now signed in."
+FILE
+end
