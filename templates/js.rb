2a0ec320c2151f2638dbb5db1c09116370b1a392
Collin Schaafsma <collin@quickleft.com>
2010-10-25 23:48:06 -0600

    remove all javascript_expansions because we are using jammit

--- templates/js.rb
+++ templates/js.rb
@@ -3,10 +3,4 @@ get "http://ajax.googleapis.com/ajax/libs/jquery/1.4.3/jquery.min.js",  "public/
 get "http://github.com/rails/jquery-ujs/raw/master/src/rails.js", "public/javascripts/rails.js"
 get "http://html5shiv.googlecode.com/svn/trunk/html5.js", "public/javascripts/shiv.js"
 
-inject_into_file 'config/application.rb', :after => "# JavaScript files you want as :defaults (application.js is always included).\n" do
-<<-RUBY
-    config.action_view.javascript_expansions[:defaults] = %w(jquery rails)
-RUBY
-end
-
 gsub 'gsub_file', /config.action_view.javascript_expansions[:defaults] = %w\(\)/, ''
