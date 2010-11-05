c379b546d9c1e99cbba57ae4e375f58967c59bed
Loïc Minaudier <loic.minaudier@gmail.com>
2010-11-05 11:25:24 +0100

    Get rails.js file from github 'jquery-ujs' project through https instead of http

--- templates/js.rb
+++ templates/js.rb
@@ -1,6 +1,6 @@
 
 get "http://ajax.googleapis.com/ajax/libs/jquery/1.4.3/jquery.min.js",  "public/javascripts/jquery.js"
-get "http://github.com/rails/jquery-ujs/raw/master/src/rails.js", "public/javascripts/rails.js"
+get "https://github.com/rails/jquery-ujs/raw/master/src/rails.js", "public/javascripts/rails.js"
 get "http://html5shiv.googlecode.com/svn/trunk/html5.js", "public/javascripts/shiv.js"
 run 'rm public/javascripts/application.js'
 
@@ -57,3 +57,4 @@ create_file 'public/javascripts/core.js' do
 })(jQuery);
 FILE
 end
+
