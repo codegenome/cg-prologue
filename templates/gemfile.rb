90f0bbb3aa3f5f63081f980afc8e50d37d90c280
Collin Schaafsma <collin@quickleft.com>
2010-11-12 11:55:07 -0700

    changing deps in gem file that gets generated to use ~>

--- templates/gemfile.rb
+++ templates/gemfile.rb
@@ -3,7 +3,7 @@ create_file 'Gemfile', "source 'http://rubygems.org'\n"
 gem "rails", "~> 3.0.0"
 gem "sqlite3-ruby", :require => "sqlite3"
 if ENV['PROLOGUE_AUTH']
-  gem 'devise', ">= 1.1.3"
+  gem 'devise', "~> 1.1.3"
 end
 if ENV['PROLOGUE_ROLES']
   gem 'cancan'
@@ -12,7 +12,7 @@ gem "hoptoad_notifier"
 gem "jammit"
 gem "friendly_id", "~> 3.1"
 gem "will_paginate", "~> 3.0.pre2"
-gem "haml", ">= 3.0.21"
+gem "haml", "~> 3.0.21"
 gem "rails3-generators", :group => :development
 gem "hpricot", :group => :development
 gem "ruby_parser", :group => :development
@@ -26,7 +26,7 @@ gem "thin", :group => [:test, :cucumber, :development]
 gem "cucumber", :group => [:cucumber]
 gem "database_cleaner", :group => [:test, :cucumber]
 gem "cucumber-rails", :group => [:cucumber]
-gem "capybara", "0.4.0", :group => [:cucumber]
+gem "capybara", "~> 0.4.0", :group => [:cucumber]
 gem "launchy", :group => [:cucumber]
 gem "timecop", :group => [:test, :cucumber]
 gem "pickle", :group => [:test, :cucumber]
