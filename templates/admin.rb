da4bfb87b488be47ad18e4b4c5e7d8dc33764d22
Collin Schaafsma <collin@quickleft.com>
2010-12-16 21:18:36 -0700

    adding before_filter :verify_admin to admin base controller so only admins can roll there proper

--- templates/admin.rb
+++ templates/admin.rb
@@ -11,6 +11,12 @@ create_file 'app/controllers/admin/base_controller.rb' do
 class Admin::BaseController < ApplicationController
   layout 'admin'
   before_filter :authenticate_user!  
+  before_filter :verify_admin
+
+private
+  def verify_admin
+    redirect_to root_url unless current_user.role? :admin
+  end
 end
 RUBY
 end
