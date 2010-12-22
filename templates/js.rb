58e3b03b4745c5b15cbee9f29dc457f3ebe401df
Collin Schaafsma <collin@quickleft.com>
2010-12-21 20:46:17 -0700

    jquery 1.4.4

--- templates/js.rb
+++ templates/js.rb
@@ -1,5 +1,5 @@
 
-get "http://ajax.googleapis.com/ajax/libs/jquery/1.4.3/jquery.min.js",  "public/javascripts/jquery.js"
+get "http://ajax.googleapis.com/ajax/libs/jquery/1.4.4/jquery.min.js",  "public/javascripts/jquery.js"
 get_remote_https_file "https://github.com/rails/jquery-ujs/raw/master/src/rails.js", "public/javascripts/rails.js"
 get "http://html5shiv.googlecode.com/svn/trunk/html5.js", "public/javascripts/shiv.js"
 run 'rm public/javascripts/application.js'
