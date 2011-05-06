71daa7730912b01dcbfecf99f06f29a94caffa94
Ryan Cook <ryan@quickleft.com>
2011-05-06 16:48:40 -0600

    specify appropriate rails version as well

--- templates/async/lib/gemfile.rb
+++ templates/async/lib/gemfile.rb
@@ -1,6 +1,6 @@
 run 'rm Gemfile'
 create_file 'Gemfile', "source 'http://rubygems.org'\n"
-gem "rails"
+gem "rails" , "~> 3.0.0"
 gem "database_cleaner", :group => :test
 gem "rspec-rails", "~> 2.0.0", :group => :test
 gem "sqlite3-ruby", :require => "sqlite3", :group => [:test, :development]
